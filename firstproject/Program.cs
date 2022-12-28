00865a4bc14797ef37ec089914e136ba939d2048
ngarcha <ngarcha@GWD-OFC02>
2022-12-28 12:07:22 -0600

    230: added interfaces

--- firstproject/Program.cs
+++ firstproject/Program.cs
@@ -6,12 +6,36 @@ using System.Threading.Tasks;
 
 namespace firstproject
 {
+    public interface IWood
+    {
+        void Sum(int a, int b);
+    }
+    public interface IGlass
+    {
+        void Mul(int a, int b);
+    }
+
+    class Ghar : IWood, IGlass
+    {
+        public void Mul(int a, int b)
+        {
+            Console.WriteLine($"hello from Mul {a} and hello from {b}", a.ToString(), b.ToString());
+        }
+
+        public void Sum(int a, int b)
+        {
+            Console.WriteLine($"hello from Sum {a} and hello from {b}", a.ToString(), b.ToString());
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello!");
-
+            Ghar obj = new Ghar();
+            obj.Mul(1, 2);
+            obj.Sum(2, 3);
         }
     }
 }
